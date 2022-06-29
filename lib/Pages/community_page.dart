462c067a39ebb05e1aba0f2581a19ac8a7211964
Arash Rezaali <tmbrks78@gmail.com>
2022-06-29 15:06:43 +0430

    adjusting radius of avatar,put star Icon

--- lib/Pages/community_page.dart
+++ lib/Pages/community_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class CommunityPage extends StatelessWidget {
+class CommunityPage extends StatefulWidget {
   const CommunityPage({Key? key}) : super(key: key);
+
+  @override
+  State<CommunityPage> createState() => _CommunityPageState();
+}
+
+class _CommunityPageState extends State<CommunityPage> {
+List<String> list=['1','2','3','4','5','6','7','8','9','10'];
 
   @override
   Widget build(BuildContext context) {
@@ -13,18 +20,34 @@ class CommunityPage extends StatelessWidget {
           itemCount: 10,
           itemBuilder: (context, index) {
             return ListTile(
+              onTap: () {
+                Navigator.pushNamed(context, '/');
+              },
               leading: CircleAvatar(
+                radius: 15,
                 backgroundImage: AssetImage(
                   "image.png",
                 ),
               ),
               tileColor: Colors.black,
-              title: Text('Title'),
+              title: Text(
+                '${list[index]}',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               textColor: Colors.white,
-              onTap: () {
-                Navigator.pushNamed(context, '/');
-              },
-
+              trailing: GestureDetector(
+                child: Icon(
+                  Icons.star_border_outlined,
+                  color: Colors.white,
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, '/');
+                },
+              ),
             );
           },
         ),
