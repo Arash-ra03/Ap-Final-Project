ec39cbf4652c7db3e9d4a99444e9692e5677eebd
Arash Rezaali <tmbrks78@gmail.com>
2022-06-29 14:41:34 +0430

    add onTap to listTile

--- lib/Pages/community_page.dart
+++ lib/Pages/community_page.dart
@@ -7,10 +7,28 @@ class CommunityPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(
+                  "image.png",
+                ),
+              ),
+              tileColor: Colors.black,
+              title: Text('Title'),
+              textColor: Colors.white,
+              onTap: () {
+                Navigator.pushNamed(context, '/');
+              },
 
+            );
+          },
+        ),
+      ),
     );
   }
 }
-
-
