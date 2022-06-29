990ee6adb64f002742a40f49dac71f00e0cf6bb8
Arash Rezaali <tmbrks78@gmail.com>
2022-06-29 12:21:00 +0430

    community_page added

---
+++ lib/Pages/community_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class CommunityPage extends StatelessWidget {
+  const CommunityPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+
+    );
+  }
+}
+
+
