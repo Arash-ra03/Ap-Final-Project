72d60184a7b30e616918f01ef78b6da74c08dc8c
Arash Rezaali <tmbrks78@gmail.com>
2022-06-30 13:29:00 +0430

    make post_detail_page.dart

---
+++ lib/Pages/post_detail_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PostDetail extends StatelessWidget {
+  const PostDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: Colors.black,
+      bottomSheet: Container(
+        color: Colors.black87,
+        padding: EdgeInsets.only(left: 10, right: 10, bottom: 10, top: 10),
+        child: TextField(
+          style: TextStyle(color: Colors.grey),
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            filled: true,
+            fillColor: Colors.black26,
+            hintText: 'Add a comment',
+            hintStyle: TextStyle(
+              color: Colors.grey,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide: BorderSide(
+                color: Colors.black26,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide: BorderSide(
+                color: Colors.black26,
+              ),
+            ),
+            prefixIcon: Icon(
+              Icons.comment,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+
+    );
+  }
+}
