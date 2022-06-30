d92cc998113e22e87e3953a0e75d2655a35c52f1
Arash Rezaali <tmbrks78@gmail.com>
2022-06-30 15:30:48 +0430

    put sizedBox and Text

--- lib/Pages/community_detail_page.dart
+++ lib/Pages/community_detail_page.dart
@@ -7,7 +7,7 @@ class CommunityDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.grey[900],
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(170),
         child: AppBar(
@@ -23,19 +23,73 @@ class CommunityDetail extends StatelessWidget {
           ),
         ),
       ),
-
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: CircleAvatar(
-          radius: 45,
-          backgroundColor: Colors.grey,
-          child: CircleAvatar(
-            radius: 40,
-            child: ClipRect(
-              child: Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/F1.svg/2560px-F1.svg.png"),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: 43,
+                  backgroundColor: Colors.grey,
+                  child: CircleAvatar(
+                    radius: 40,
+                    child: ClipRect(
+                      child: Image.network(
+                          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/F1.svg/2560px-F1.svg.png"),
+                    ),
+                    backgroundColor: Colors.black,
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  'Community Name',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
-            backgroundColor: Colors.black,
-          ),
+            Padding(
+              padding: const EdgeInsets.only(left: 18, top: 7),
+              child: SizedBox(
+                width: double.infinity,
+                child: Text(
+                  'About:',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(18),
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.black26,
+                ),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 120,
+                  child: Text(
+                    'This is f1 Community for f1 fans!!',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.white,
+
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
